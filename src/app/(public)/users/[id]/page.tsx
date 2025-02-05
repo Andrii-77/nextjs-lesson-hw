1196d0048b19db151378c2d6566b53412620ee21
Andrii-77 <andrijsmaga@gmail.com>
2025-02-05 09:53:46 +0200

    Commit and push lesson2 part_2 forward-data (pass data between pages).

--- src/app/(public)/users/[id]/page.tsx
+++ src/app/(public)/users/[id]/page.tsx
@@ -1,23 +1,36 @@
 import {FC} from "react";
 import {Metadata} from "next";
+import {SearchParams} from "next/dist/server/request/search-params";
+import {IUser} from "@/models/IUser";
 
 type Props = {
-    params: {id: string};
+    params: Promise<{ id: string }>;
+    searchParams: Promise<SearchParams>;
+    //так інтелідж не буде давати помилку і казати що ми не працюємо з промісом.
 }
 
-export const generateMetadata = async ({params}: Props):Promise<Metadata> => {
+export const generateMetadata = async ({params}: Props): Promise<Metadata> => {
     const {id} = await params;
     return {
         title: 'User Page Title' + id,
     }
 }
 
-const UserPage: FC<Props> = async ({params}) => {
-    const {id} = await params;
+const UserPage: FC<Props> = async ({searchParams}) => {
+
+    let obj = null;
+    const {data} = await searchParams;
+    if (typeof data === "string") {
+        obj = JSON.parse(data) as IUser;
+    }
+    // цю всю логіку треба виносити в helper (допоміжний сервіс)!!! Це в пейджі не робиться.
 
     return (
         <div>
-            User Page Content {id}
+            {
+                obj && <>User Page Content {obj.id} {obj.name}</>
+                // будуємо фрагмент, перевіряємо обєкт на присутність, і якщо він є то робим необхідну дію.
+            }
         </div>
     );
 };
--- src/components/users/UsersComponent.tsx
+++ src/components/users/UsersComponent.tsx
@@ -9,9 +9,20 @@ export const UsersComponent = async () => {
         <div>
             {
                 users.map((user) => <div key={user.id}>
-                    <Link href={'/users/' + user.id.toString()}>{user.id} {user.name}</Link>
+                    <Link href={{
+                        pathname: '/users/' + user.id.toString(),
+                        query: {data: JSON.stringify(user)}
+                    }}>{user.id} {user.name}</Link>
                 </div>)
             }
         </div>
     );
 };
+
+//query: {name:user.name} - це коли передавати кожну хар-ку окремо.
+
+//query: {...user} - це щоб отримати відразу всі хар-ки.
+
+//query: {data: JSON.stringify(user)} - тепер серчпарамс буде складатись з хар-ки data і застрінгіфаєного вашого
+// обєкту, а на сторінці користувача їх вже можна спокійно перетворити назад: JSON.parse(sp.data). Отримуєте обєкт.
+// Після цього цим обєктом можете працювати як вам необхідно.
